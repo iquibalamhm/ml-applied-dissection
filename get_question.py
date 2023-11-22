308c7216ae97dc48824e3054bc613c62086a9b68
iquibalamhm <iquibalamhm@gmail.com>
2023-11-21 20:50:53 -0500

    added prediction printing

--- get_question.py
+++ get_question.py
@@ -9,14 +9,26 @@ parser = argparse.ArgumentParser()
 parser.add_argument("dataset", help="Question ID to look for", type=str
                     , choices=['siq2', 'how2qa'])
 parser.add_argument("qid", help="Question ID to look for", type=int)
+parser.add_argument("blackout_percent", help="Percentage of words to blackout", type=int, default=100)
 args = parser.parse_args()
 
 dataset_name = args.dataset
 dataset_folder = 'SIQ2' if dataset_name == 'siq2' else 'How2QA'
 
-# Open and read JSON file
-with open(f'results/ft_{dataset_name}_100/{dataset_name}_val.json') as json_file:
+# Open and read JSON file for 0 blackout percent
+with open(f'results/ft_{dataset_name}_{str(0)}/{dataset_name}_val.json') as json_file:
     json_data = json.load(json_file)
+    # Extract 'pred' and 'acc' from JSON data
+    pred_0 = json_data[str(args.qid)]['pred']
+    acc_0 = json_data[str(args.qid)]['acc']
+
+# Open and read JSON file for blackout percent given in args
+if args.blackout_percent != 0:
+    with open(f'results/ft_{dataset_name}_{str(args.blackout_percent)}/{dataset_name}_val.json') as json_file:
+        json_data = json.load(json_file)
+        # Extract 'pred' and 'acc' from JSON data
+        pred_arg = json_data[str(args.qid)]['pred']
+        acc_arg = json_data[str(args.qid)]['acc']
 
 # Open and read CSV file
 with open(f'datasets/{dataset_folder}/public_val.csv', newline='') as csv_file:
@@ -38,3 +50,10 @@ print(f"Question: {question}")
 print(f"Options: {options}")
 print(f"Correct Answer ID: {correct_answer_id}")
 print(f"Subtitles: {subtitles[video_id]}")
+#Print 'pred' and 'acc' with 0 blackout percent
+print(f"Prediction with {str(0)} blackout percentage: {pred_0}")
+print(f"Accuracy with {str(0)} blackout percentage: {acc_0}")
+if args.blackout_percent != 0:
+    # Print 'pred' and 'acc' with blackout percent given in args
+    print(f"Prediction with {str(args.blackout_percent)} blackout percentage: {pred_arg}")
+    print(f"Accuracy with {str(args.blackout_percent)} blackout percentage: {acc_arg}")
